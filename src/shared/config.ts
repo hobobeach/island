b7146e67644b38c9ebbb059a8839e1a25b0990bc
Erik Johnson <erik@erikaugust.com>
2026-05-12 12:17:53 -0400

    Renames in config

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,11 +1,11 @@
 export const config = {
-  name: 'Express Base',
+  name: 'Island',
   layout: 'default',
   version: '0.1.0',
   emoji: '🏖️',
   image: '',
   email: '',
-  url: '', 
+  url: 'https://island0.com', 
   title: '',
   description: '',
   twitter: '',
